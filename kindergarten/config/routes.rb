8bcebe3d45b7e650a12f9086b808410ea5e9786a
kubawez <kuba.wezykowski@gmail.com>
2016-04-16 15:56:45 +0000

    Unified the layout. Changed texts to Polish.

--- kindergarten/config/routes.rb
+++ kindergarten/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :children
   root 'static_pages#index'
   get 'about' => 'static_pages#about'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+  resources :children
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
